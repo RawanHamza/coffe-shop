4dfa8b98c6be4bfb1d6ce0bec427e6b87292ccd0
Rawan Emad Hamza <62294577+RawanHamza@users.noreply.github.com>
2020-06-07 23:19:23 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,25 +1,27 @@
-// Making menu in mobile view
-let listButton = document.querySelector('.toggle-button');
-let navItems = document.querySelector('.nav-items');
-let closeList = document.querySelector('body');
-if(window.innerWidth<='776')
-	{
-listButton.addEventListener('click',  function(e){
-    navItems.style.display='block';
-    console.log(" block");
-  e.stopPropagation();
- });
-window.addEventListener('click',function(){
-  navItems.style.display='none';
-  console.log("none");
+//create dynamic navbar
+var listItem = [{label : "Home", href : "#Header" ,class: "active"},
+               {label : "News & Articles", href : "#News"},{label : "Cafes", href : "#Cafes"},{label : "Newsletter", href : "#Newsletter"},{label : "Help", href : "#Help"}];
+var array = listItem.map(function(listI) {
+  return "<a href="+ listI.href + ">"+ listI.label + "</a>";
 })
+var listContainer = document.querySelector("#mainNav");
+array.forEach(listItem => listContainer.innerHTML+=listItem);
 
-	}
 
+// Making menu in mobile view
+ const navSlide= () => {
+   const burger = document.querySelector(".burger");
+   const nav= document.querySelector(".nav-links");
+   burger.addEventListener('click',() => {nav.classList.toggle('nav-active');
+   burger.classList.toggle('toggle');
+                                         
+  });
+ }
+ navSlide();
 
 //It should be clear which section is being viewed while scrolling through the page. 
 function onScroll(event){
-  var sections = document.querySelectorAll('#menu a');
+  var sections = document.querySelectorAll('#mainNav a');
   var scrollPos = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
   
   for( var i = 0; i < sections.length; i++ ){
@@ -27,7 +29,7 @@ function onScroll(event){
     var val = currLink.getAttribute('href');
     var refElement = document.querySelector(val);
       if( refElement.offsetTop <= scrollPos && ( refElement.offsetTop + refElement.offsetHeight > scrollPos)){
-        document.querySelector('#menu ul li a').classList.remove('active');
+        document.querySelector('#mainNav a').classList.remove('active');
         currLink.classList.add('active');
       }else{
          currLink.classList.remove('active');
